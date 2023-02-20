b5ed7d41cd44753b132214b1b4c62a74c56d087a
Anton Pasyuk <atadrakula25@yandex.ru>
2023-02-20 14:49:05 +0300

    Develop/finished. Review 1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let editButtonPopup = document.querySelector(".profile__button-edit");
 let editPopup = document.querySelector(".popup");
 let editProfileClose = document.querySelector(".popup__close");
-let userName = document.querySelector(".popup__input-name");
-let userActivity = document.querySelector(".popup__input-activity");
+let userName = document.querySelector(".popup__input_type_name");
+let userActivity = document.querySelector(".popup__input_type_activity");
 let profileName = document.querySelector(".profile__name");
 let profileActivity = document.querySelector(".profile__activity");
-let formElement = document.querySelector(".popup__container");
-let saveProfile = document.querySelector(".popup__submit");
+let submitFormAndSave = document.querySelector(".popup__container");
+
 
 editButtonPopup.addEventListener("click", function () {
   userName.value = profileName.textContent;
@@ -18,23 +18,15 @@ editProfileClose.addEventListener("click", function () {
   closePopup(editPopup);
 });
 
-saveProfile.addEventListener("click", function() {
-  profileName.textContent = userName.value;
-  profileActivity.textContent = userActivity.value;
-  closePopup(editPopup);
-});
-
-
-formElement.addEventListener("submit", handleFormSubmit);
+submitFormAndSave.addEventListener("submit", handleFormSubmit);
 
 function openPopup(popup) {
-  popup.classList.add("popup__opened");
+  popup.classList.add("popup_opened");
 }
 
 function closePopup(popup) {
-  popup.classList.remove('popup__opened');
+  popup.classList.remove('popup_opened');
 }
-
 
 function handleFormSubmit(event) {
   event.preventDefault();
@@ -42,4 +34,5 @@ function handleFormSubmit(event) {
   let jobInput = userActivity.value;
   profileName.textContent = nameInput;
   profileActivity.textContent = jobInput;
+  closePopup(editPopup);
 }
