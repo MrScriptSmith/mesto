f189ceeac341e87406bd3f94a1cc47ff44a0aaf4
Anton Pasyuk <atadrakula25@yandex.ru>
2023-03-01 22:09:48 +0300

    Developed/2 - continue

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,66 @@
-let editButtonPopup = document.querySelector(".profile__button-edit");
-let editPopup = document.querySelector(".popup");
-let editProfileClose = document.querySelector(".popup__close");
-let userName = document.querySelector(".popup__input_type_name");
-let userActivity = document.querySelector(".popup__input_type_activity");
-let profileName = document.querySelector(".profile__name");
-let profileActivity = document.querySelector(".profile__activity");
-let submitFormAndSave = document.querySelector(".popup__form");
+const editButtonPopup = document.querySelector('.profile__button-edit');
+const editPopup = document.querySelector('#popup-edit');
+const editProfileClose = document.querySelector('#popup-edit-close');
+const userName = document.querySelector('#popup-edit-input-name');
+const userActivity = document.querySelector('#popup-edit-input-activity');
+const profileName = document.querySelector('.profile__name');
+const profileActivity = document.querySelector('.profile__activity');
+const submitFormAndSave = document.querySelector('#popup-edit-form');
+const cardsContainer = document.querySelector('.places__cards');
+const cardsTemplate = document.querySelector('#cards').content;
 
 
-editButtonPopup.addEventListener("click", function () {
+const initialCards = [
+  {
+    name: 'Храм Лемпуянг',
+    link: './images/Bally.jpg'
+  },
+  {
+    name: 'Будапешт',
+    link: './images/Budapest.jpg'
+  },
+  {
+    name: 'Бурдж-Халифа',
+    link: './images/Dubai.jpg'
+  },
+  {
+    name: 'Пирамиды',
+    link: './images/Egypt.jpg'
+  },
+  {
+    name: 'Прага',
+    link: './images/Prague.jpg'
+  },
+  {
+    name: 'Золотой мост в Дананге',
+    link: './images/Vietnam.jpg'
+  }
+];
+
+initialCards.forEach((card) => {
+  const cardTemplate = cardsTemplate.querySelector('.cards').cloneNode(true);
+  const nameOfTemplate = cardTemplate.querySelector('.cards__name');
+  const imageOfTemplate = cardTemplate.querySelector('.cards__img');
+  nameOfTemplate.textContent = card.name;
+  imageOfTemplate.alt = card.name;
+  imageOfTemplate.src = card.link;
+  cardsContainer.append(cardTemplate);
+});
+
+editButtonPopup.addEventListener('click', function () {
   userName.value = profileName.textContent;
   userActivity.value = profileActivity.textContent;
   openPopup(editPopup);
 });
 
-editProfileClose.addEventListener("click", function () {
+editProfileClose.addEventListener('click', function () {
   closePopup(editPopup);
 });
 
-submitFormAndSave.addEventListener("submit", handleFormSubmit);
+submitFormAndSave.addEventListener('submit', handleFormSubmit);
 
 function openPopup(popup) {
-  popup.classList.add("popup_opened");
+  popup.classList.add('popup_opened');
 }
 
 function closePopup(popup) {
