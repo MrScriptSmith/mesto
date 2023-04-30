6b858b50405c97c8aa32134c4ae3840f2c40a13c
Anton Pasyuk <atadrakula25@yandex.ru>
2023-04-30 14:34:39 +0300

    Develop/s8 finished

--- src/scripts/utils/constants.js
+++ src/scripts/utils/constants.js
@@ -1,3 +1,11 @@
+const imageBally = new URL('../../images/Bally.jpg', import.meta.url);
+const imageBudapest = new URL('../../images/Budapest.jpg', import.meta.url);
+const imageDubai = new URL('../../images/Dubai.jpg', import.meta.url);
+const imageEgypt = new URL('../../images/Egypt.jpg', import.meta.url);
+const imagePrague = new URL('../../images/Prague.jpg', import.meta.url);
+const imageVietnam = new URL('../../images/Vietnam.jpg', import.meta.url);
+
+
 // Константы для модуля card.js, index.js
 const press = 'ontouchstart' in window ? 'touchstart' : 'click';
 
@@ -5,27 +13,27 @@ const press = 'ontouchstart' in window ? 'touchstart' : 'click';
 const initialCards = [
   {
     name: 'Храм Лемпуянг',
-    link: './images/Bally.jpg',
+    link: imageBally,
   },
   {
     name: 'Будапешт',
-    link: './images/Budapest.jpg',
+    link: imageBudapest,
   },
   {
     name: 'Бурдж-Халифа',
-    link: './images/Dubai.jpg',
+    link: imageDubai,
   },
   {
     name: 'Пирамиды',
-    link: './images/Egypt.jpg',
+    link: imageEgypt,
   },
   {
     name: 'Прага',
-    link: './images/Prague.jpg',
+    link: imagePrague,
   },
   {
     name: 'Золотой мост в Дананге',
-    link: './images/Vietnam.jpg',
+    link: imageVietnam,
   },
 ];
 
